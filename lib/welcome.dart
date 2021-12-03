2c4ad069d2eb8723c0e645a7ef59644fe8bcc520
zayyan <zayyanshafiq0@gmail.com>
2021-12-03 19:53:08 +0500

    first commit

---
+++ lib/welcome.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+/*
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Welcome!'),
+    );
+  }
+}*/
+class Welcome extends StatefulWidget {
+  @override
+  _WelcomeState createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('WELCOME  '),
+    );
+  }
+}
